6e587d317e20b866d82cf1adba3d0fcbd40daa14
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-07 11:39:09 +0200

    Working With File System In PHP

---
+++ files.php
@@ -0,0 +1,26 @@
+<?php
+    /* WORKING WITH FILESYSTEM */
+
+    // $dir = scandir(__DIR__);
+    // var_dump($dir);
+
+    // mkdir('foo') //this creates a directory
+    // rmdir('foo') //removes a directory
+
+    // if(file_exists('foo.txt')){
+    //     echo filesize('foo.txt');
+
+    //     file_put_contents('foo.txt', 'Some content'); //adds text into a file
+    //     clearstatcache(); //clears the cache
+    //     echo filesize('foo.txt');
+    // } else {
+    //     echo 'File not found';
+    // }
+
+    // $file = fopen('foo.txt', 'r'); //opens the file for reading
+
+    // while(($line = fgets($file)) !== false){ //reading the file line by line until it return false
+    //     echo $lien . '<br />';
+    // }
+    // fclose($file); //closes the file
+?>
